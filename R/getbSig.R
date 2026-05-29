23d56fa7ebf2dd51affd4dd890b08d394ba1c3ae
Emily Ouyang <emilyouyang@Emilys-MacBook-Pro-2.local>
2026-05-29 10:17:04 -0700

    add other association structures

--- R/getbSig.R
+++ R/getbSig.R
@@ -10,7 +10,11 @@ getbSig <- function(bSig, data){
   names(data) <- c("beta", "gamma1", "gamma2", "alphaList",
                    "sigma", "Z", "X", "Y", "Sig",
                    "CH01", "CH02",
-                   "HAZ01", "HAZ02", "Wcmprsk", "Wx")
+                   "HAZ01", "HAZ02", "Wcmprsk", "Wx", "landmark", "latent", "s")
+  
+  landmark <- data$landmark
+  latent <- data$latent
+  s <- data$s
   
   Y <- data$Y
   X <- data$X # update so both biomarkers accountted for
@@ -33,6 +37,8 @@ getbSig <- function(bSig, data){
   # survival
   CH01 <- data$CH01
   CH02 <- data$CH02
+  
+
   HAZ01 <- data$HAZ01
   HAZ02 <- data$HAZ02
   Wcmprsk <- data$Wcmprsk
@@ -57,6 +63,7 @@ getbSig <- function(bSig, data){
   q = sum(pREvec)
   
   index = 0
+  
   b <- vector("list", length(pREvec))
   bfull <- c()
   for(p in 1:length(pREvec)){
@@ -95,7 +102,7 @@ getbSig <- function(bSig, data){
       alpha2g <- alpha2[[g]] # alpha2
     }else{
       alpha1g <- alpha1
-      alpha2g <- alpha2
+      alpha2g <- alpha26
     }
     
     # log likelihood part 1
@@ -106,32 +113,59 @@ getbSig <- function(bSig, data){
     # double check if it is squared
     
     # sum alpha'b
-    sum.alpha1i <- sum.alpha1i + t(alpha1g) %*% bi #alpha1
-    sum.alpha2i <- sum.alpha2i + t(alpha2g) %*% bi #alpha2
+    
+    if(data$latent == "shared"){
+      sum.alpha1i <- sum.alpha1i + t(alpha1g) %*% bi #alpha1
+      sum.alpha2i <- sum.alpha2i + t(alpha2g) %*% bi #alpha2
+    } else if(latent == "presentlp"){
+          # if(length(alpha1g)== 1){
+            sum.alpha1i <- sum.alpha1i + alpha1g %*% Zi[nrow(Zi),] %*% bi #alpha1
+            sum.alpha2i <- sum.alpha2i + alpha2g %*% Zi[nrow(Zi),] %*% bi #alpha2
+          # }else if(length(alpha1g) == 2){
+          #   sum.alpha1i <- sum.alpha1i + t(alpha1g) %*% rbind(Zi[nrow(Zi),], c(0,1)) %*% bi #alpha1
+          #   sum.alpha2i <- sum.alpha2i + t(alpha2g) %*% rbind(Zi[nrow(Zi),], c(0,1)) %*% bi #alpha2
+          # }
+    } else if(latent == "present"){
+        sum.alpha1i <- sum.alpha1i + alpha1g %*% (Xi[nrow(Xi),] %*% betai - Zi[nrow(Zi),] %*% bi) #alpha1
+        sum.alpha2i <- sum.alpha2i + alpha2g[k] %*% (Xi[nrow(Xi),] %*% betai - Zi[nrow(Zi),] %*% bi) #alpha2
+     
+    } else{
+      # error
+    }
+
   }
+  # print("first")
+  # print(total)
+  # if (landmark) {
+  #   # CH0(s,T) = CH0(0,T) - CH0(0,s)
+  #   CH01 <- CH01 - data$CH01_s
+  #   CH02 <- CH02 - data$CH02_s
+  # } else {
+  #   CH01 <- CH01
+  #   CH02 <- CH02
+  # }
 
   
   # latent structure for each loop
   latent1 <- as.matrix(sum.alpha1i, nrow = 1)
   latent2 <- as.matrix(sum.alpha2i, nrow = 1)
   
-  
-  
   total <- total + CH01 * exp(Wx %*% gamma1 + latent1) + ## part 2 change this part
     CH02 * exp(Wx %*% gamma2 + latent2) +
     0.5 * q *log(2*pi) +
     0.5*log(det(Sig)) + t(bfull) %*% solve(Sig) %*% bfull / 2  # part 3
   
-  
+  # print(total)
   
   if (Wcmprsk == 1) {
-    # should be HAZ?, double check though
     total <- total - log(HAZ01) - (Wx %*% gamma1 + latent1) # adjusts for status == 1
   }
   
   if (Wcmprsk == 2) {
     total <- total - log(HAZ02) - (Wx %*% gamma2 + latent2)  # adjusts for status == 2
   }
+  # print(total)
+  # print("end")
   
   total <- unname(total)
   
